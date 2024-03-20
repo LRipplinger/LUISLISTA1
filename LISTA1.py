af9c4c2ce122cbdc121a1562352a9a2f1ae0c3cf
LRipplinger <162515197+LRipplinger@users.noreply.github.com>
2024-03-20 10:28:18 -0300

    Add files via upload

---
+++ LISTA1.py
@@ -0,0 +1,14 @@
+# Imprimir a questão
+print("Qual é o verdadeiro nome do super-herói Batman?")
+
+# Apresentar as alternativas
+print("a) Bruce Wayne\nb) Clark Kent\nc) Peter Parker\nd) Tony Stark\ne) Steve Rogers")
+
+# Armazenar a resposta correta
+resposta_correta = 'a'
+
+# Solicitar ao usuário que digite sua resposta
+resposta_usuario = input("Digite a letra correspondente à sua resposta: ")
+
+# Exibir a resposta do usuário e a resposta correta
+print(f"Você respondeu alternativa {resposta_usuario}. A resposta correta é a alternativa {resposta_correta}.")
